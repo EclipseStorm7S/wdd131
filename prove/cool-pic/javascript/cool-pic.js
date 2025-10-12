3d6a664ed69e4533d0f7fe740177cd5cd3e27344
EclipseStorm7S <54195382+EclipseStorm7S@users.noreply.github.com>
2025-10-11 23:28:18 -0600

    Completed Cool pics pt2

---
+++ prove/cool-pic/javascript/cool-pic.js
@@ -0,0 +1,46 @@
+
+const menuButton = document.getElementById("menu-btn");
+const navBar = document.querySelector("nav");
+
+menuButton.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    navBar.classList.toggle('hide');
+    menuButton.classList.innertext('change');
+}
+
+const gallery = document.querySelector('#picgrid');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+// Code to show modal  - Use event parameter 'e'   
+    console.log(e.target);
+    const img = e.target;
+    if (img.src == null){
+        return;
+    }
+    let src = img.getAttribute('src');
+    let alt = img.getAttribute('alt');
+    let fullsize = src.replace('sm','full');
+
+    modalImage.src = fullsize;
+    modalImage.alt = alt;
+
+    modal.showModal();
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
